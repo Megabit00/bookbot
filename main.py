4427316a5b99253d4ed83ef2c284218bd2a81d66
patrick <155462864+Megabit00@users.noreply.github.com>
2025-09-17 21:39:15 +0300

    D. done

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from stats import count_stings
-from stats import count_char
-
+from stats import count_stings, count_char, chars_to_sorted_list
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
-
 def main():
-    
     book_path = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
 
     text = get_book_text(book_path)
-    
-    the_dict = count_char(text)
-
-
     count_stings(text)
-    print(the_dict)
-    
+
+    print("--------- Character Count -------")
+    chars_dict = count_char(text)
+    sorted_chars = chars_to_sorted_list(chars_dict)
+
+    for item in sorted_chars:
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,26 +1,32 @@
-
-
 def count_stings(text):
     num_words = len(text.split())
-    print(f"{num_words} words found in the document")
-
-
-
+    print(f"Found {num_words} total words")
+    return num_words
 
 
 def count_char(teksti):
     teksti_low_case = teksti.lower()
     dict = {}
     for char in teksti_low_case:
-
-        keys = dict.keys()
-        if char in keys:
+        if char in dict:
             dict[char] += 1
         else:
             dict[char] = 1
-        
-
     return dict
 
+
+def sort_on(item):
+    return item["num"]
+
+
+def chars_to_sorted_list(chars_dict):
+    list_of_dicts = []
+    for char, count in chars_dict.items():
+        if char.isalpha():  # keep only letters
+            list_of_dicts.append({"char": char, "num": count})
+
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
     
-    
+
